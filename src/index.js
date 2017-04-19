3cda7cbb655b41953976df6f997504b6ce3c295e
Kevin Leugers <kevin+bit@talmetrix.com>
2017-04-18 20:51:19 -0400

    added vides list

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCpvkHm63sa8I7c_FkbxTL8NqBEVPWufsc';
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     );
